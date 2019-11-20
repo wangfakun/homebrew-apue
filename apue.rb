888112df5b73f742b107f90f26f5f0cd541f0894
wangfakun <449288317@qq.com>
2019-11-21 06:29:49 +0800

    add new formula apue

---
+++ apue.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Apue < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/wangfakun/apue/raw/master/apue-1.0.tar.gz"
+  sha256 "0aa17eba27bebda0b9e24983565d65bcbb0ad03a62551075f66521d631510bfb"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+     lib.install "libapue.a"
+     include.install "apue.h"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test apue`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
